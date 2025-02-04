ab8c90be55cbb34b0705396ab7474d7e58c9cce0
Aakash Sah <aakashsah1436@gmail.com>
2025-02-04 16:55:40 +0530

    Layout has been postted

---
+++ pg_mgmt/src/Components/layout.jsx
@@ -0,0 +1,23 @@
+
+import React from 'react'
+
+const Layout = () => {
+    return (
+      <>
+      <div class="container">
+        <nav>Navbar</nav>
+        <main>MAin</main>
+        <div class="sidebar">sidebar</div>
+        <div class="box1">Box1</div>
+        <div class="box3">Box2</div>
+        <div class="box2">Box3</div>
+        <footer>Footer</footer>
+
+    </div>
+
+      </>
+  )
+}
+
+export default  Layout    
+
